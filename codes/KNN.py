1d6f7622b909ebcbf26bb771f6b18c3257dbc4dd
MeGaCrazy <mikaelmonuir@gmail.com>
2018-04-25 20:59:43 +0200

    Edit Repesentation of images

--- codes/KNN.py
+++ codes/KNN.py
@@ -1,3 +1,11 @@
 from codes.Preprocess import *
+from sklearn.utils import shuffle
+from sklearn.cross_validation import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 X, y = data_reader()
+X, y = shuffle(X, y, random_state=2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=4)
+knn = KNeighborsClassifier(n_neighbors=6)
+knn.fit(X_train, y_train)
+print('Score Of Test :'+str(knn.score(X_test, y_test)))
--- codes/Preprocess.py
+++ codes/Preprocess.py
@@ -13,6 +13,10 @@ def top5class():
     return ret_Most_freq_dog.keys()
 
 
+def image2vec(img, size=(256, 256)):
+    return cv2.resize(image, )
+
+
 def data_reader():
     data = []
     labels = []
@@ -31,7 +35,8 @@ def data_reader():
             data.append(img)
     img_data = np.array(data)
     img_data = img_data.astype('float32')
-    img_data /= 255
+    img_data = img_data.flatten().reshape(img_data.shape[0], 256 * 256 * 3)
     label_data = np.array(labels)
     print('The Data is Ready.')
+    print('Number of m : ' + str(img_data.shape[0]) + " <<<<<>>>>> " + ' Number of Features is: ' + str(img_data.shape[1]))
     return img_data, label_data
