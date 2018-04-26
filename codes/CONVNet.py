72efc9b0a42138c1aee19db7ade80d4cee68f61d
mostafasaleh44 <mostafa.saleh423@yahoo.com>
2018-04-25 15:40:40 -1000

    convnet1

--- codes/CONVNet.py
+++ codes/CONVNet.py
@@ -5,7 +5,6 @@ Created on Tue Mar 27 21:02:50 2018
 @author: CS
 """
 
-
 from __future__ import print_function
 import numpy as np
 from codes.Preprocess import *
@@ -19,49 +18,84 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.optimizers import SGD, Adam, RMSprop
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint,CSVLogger
-import tensorflow as tf 
+import tensorflow as tf
 from keras import backend as k
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+from keras.layers import GlobalAveragePooling2D,Dropout,BatchNormalization ,Input, Dense, Convolution2D, MaxPooling2D, AveragePooling2D, ZeroPadding2D, Dropout, Flatten, merge, Reshape, Activation
+from keras.models import Model
+from keras.optimizers import SGD
+from keras.utils import np_utils
+import numpy as np
+import argparse
+#import cv2
+import scipy.io
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
+from keras.callbacks import ModelCheckpoint,CSVLogger
+import keras.backend as K
+import warnings
+warnings.filterwarnings('ignore')
 
-def categorical_hinge(y_true, y_pred):
-    pos = k.sum(y_true * y_pred, axis=-1)
-    neg =k.max((1.0 - y_true) * y_pred, axis=-1)
-    return k.mean(k.maximum(0.0, neg - pos + 1), axis=-1)
+
 
 
 # Data Preparing
-X, y, our_classes = data_reader()
-X, y = shuffle(X, y, random_state=2)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=4)
 
 batch_size = 4
 nr_classes = 10
 nr_iterations = 10
+#(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X, y, our_classes = data_reader()
+X, y = shuffle(X, y, random_state=2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=4)
 #X_train = X_train.reshape(60000, 784)
 #X_test = X_test.reshape(10000, 784)
 
 X_train /= 255
 X_test /= 255
 
-
 Y_train = np_utils.to_categorical(y_train, nr_classes)
 Y_test = np_utils.to_categorical(y_test, nr_classes)
 
-input_shape=(28,28,1)
+X_val=X_train[500:1000,:]
+Y_val=Y_train[500:1000,5:10]
+
+X_train=X_train[100:500,:]
+Y_train=Y_train[100:500,10:40]
+
+
+
+
+input_shape=(32,32,3)
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
                  activation='relu',
                  input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(20, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dense(10, activation='softmax'))
+#saved_weights_name='SVMWeights.h5'
+#model.load_weights(saved_weights_name)
+model.summary()
+
+x = model.layers[4].output
+
+x=Dense(128, activation='relu')(x)
+predictions = Dense(10, activation='sigmoid')(x)
+
+model = Model(inputs=model.input, outputs=predictions)
 
 
 
 
-X_val=X_train[500:1000,:]
-Y_val=Y_train[500:1000,:]
+for layer in model.layers[0:4]:
+    layer.trainable = False
 
 
 model.summary()
@@ -69,7 +103,9 @@ model.compile(loss='mse',
               optimizer='sgd',
               metrics=['accuracy'])
 
-saved_weights_name='SVMWeights.h5'
+
+
+
 
 checkpoint = ModelCheckpoint(saved_weights_name, 
                                      monitor='val_acc', 
